11dca980668dbff32676b5b624c0e0a89d5a7417
Salva-James Marijata <marijata05@gmail.com>
2021-11-19 14:26:06 +0000

    Add project files.

---
+++ MoETest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoETest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FilterDuplicateWords();
+        }
+
+        public static void FilterDuplicateWords()
+        {
+             string phrase = @"one two one three two one three four one two three four five six seven";
+            var list = phrase.Split(' ').ToList();
+            
+                list = list.Distinct().ToList();
+
+            foreach(var word in list)
+            {
+               Console.WriteLine(word);
+            }
+            
+ 
+        }
+    }
+}
